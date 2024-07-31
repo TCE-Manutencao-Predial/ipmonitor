df91cd1ac6fd472d152aa23aa080f95ff5ab4acb
Sevenings <lvlassis.2@gmail.com>
2024-07-31 15:14:14 -0300

    refatorado

--- ip_operations.py
+++ ip_operations.py
@@ -1,12 +1,10 @@
 import concurrent.futures
 from ping3 import ping
 
-REDE_BASE = "172.17.86."
 
-ip_list = [REDE_BASE + str(i) for i in range(1, 255)]
-ip_status_dict = {ip: "on" for ip in ip_list}
-
-def verificar_ips_sem_uso():
+def verificar_ips(rede_base: str):
+    ip_list = [rede_base + str(i) for i in range(1, 255)]
+    ip_status_dict = {ip: "on" for ip in ip_list}
 
     def verificar_ip(ip):
         if not ping(ip, timeout=2): 
@@ -21,5 +19,9 @@ def verificar_ips_sem_uso():
 
 
 
-
+if __name__ == '__main__':
+    REDE_BASE = "192.168.0."
+    ips = verificar_ips(REDE_BASE)
+    for ip in ips:
+        print(f'{ip["ip"]}: {ip["status"]}') 
 
