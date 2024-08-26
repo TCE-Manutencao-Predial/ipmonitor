9d13d655c505ebbf54535dfbe8dbf403c888ccfa
Wilson <wilsoncffilho@outlook.com>
2024-08-26 14:13:40 -0300

    mudança na lógica da API

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, jsonify
 from ip_operations import verificar_ips
 import time
+import threading
+from collections import deque
+
 app = Flask(__name__)
+
+
+check_ip = []
+
+# Rotina background para verificar os IPs a cada 30s
+def background_ip_check():
+    rede_base = '172.17.86.'
+    global check_ip
+
+    while True:
+        check_ip = verificar_ips(rede_base)
+        time.sleep(30)
+
+thread = threading.Thread(target=background_ip_check,daemon=True)
+thread.start()
 
 @app.route('/')
 def index():
@@ -9,9 +27,9 @@ def index():
 
 @app.route('/api/ip-status')
 def ip_status():
-    rede_base = '172.17.86.'
-    check_ip = verificar_ips(rede_base)
+    if not check_ip:
+        return 204
     return jsonify(check_ip)
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     app.run(debug=True)
--- ip_operations.py
+++ ip_operations.py
@@ -1,27 +1,37 @@
 import concurrent.futures
 from ping3 import ping
+from collections import deque
 
 
 def verificar_ips(rede_base: str):
     ip_list = [rede_base + str(i) for i in range(1, 255)]
-    ip_status_dict = {ip: "on" for ip in ip_list}
+    ip_history = deque(maxlen=10)
+    
+    for i in range(0,10):
+        ip_history.append("off")
+    
+    # Dicionario para armazenar o historico de status de cada IP
+    ip_status_dict = {status_history: ip_history.copy() for status_history in ip_list}
+
+    # Dicionario para armazenar o status de cada IP apos verificao
+    ip_checked = {ip: "on" for ip in ip_list}
 
     def verificar_ip(ip):
-        if not ping(ip, timeout=2): 
-            ip_status_dict[ip] = "off"
+        if ping(ip, timeout=6): 
+            ip_status_dict[ip].append("on")
         
     with concurrent.futures.ThreadPoolExecutor(max_workers=255) as executor:
         executor.map(verificar_ip, ip_list)
-        
-    ip_status_list = [{"ip": ip, "status": status} for ip, status in ip_status_dict.items()]
+
+    for ip in ip_list:
+            if ip_status_dict[ip].count("on") == 0:
+                ip_checked[ip] = "off"
+            else:
+                ip_checked[ip] = "on"
+                
+    ip_status_list = [{"ip": ip, "status": status} for ip, status in ip_checked.items()]
 
     return ip_status_list
 
-
-
-if __name__ == '__main__':
-    REDE_BASE = "192.168.0."
-    ips = verificar_ips(REDE_BASE)
-    for ip in ips:
-        print(f'{ip["ip"]}: {ip["status"]}') 
+    
 
