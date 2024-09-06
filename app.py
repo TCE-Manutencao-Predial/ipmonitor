449f7629a4c0fc60b03cf3a9b4675ea28959a2c4
Wilson <wilsoncffilho@outlook.com>
2024-09-06 16:17:59 -0300

    adding select vlan port

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-from flask import Flask, render_template, jsonify, make_response
+from flask import Flask, render_template, jsonify, make_response, request
 from ip_operations import verificar_ips
 import time
 import threading
-from collections import deque
 
 app = Flask(__name__)
 
-
 check_ip = []
+background_thread = None
+stop_event = threading.Event()
 
-# Rotina background para verificar os IPs a cada 30s
-def background_ip_check():
-    rede_base = '172.17.86.'
+# Rotina background para verificar os IPs
+def background_ip_check(vlan):
     global check_ip
+    rede_base = '172.17.' + vlan + '.'
 
-    while True:
+    while not stop_event.is_set():  # Usa o evento de parada para verificar se a thread deve parar
         check_ip = verificar_ips(rede_base)
-        time.sleep(30)
-
-thread = threading.Thread(target=background_ip_check,daemon=True)
-thread.start()
+        stop_event.wait(30) 
 
 @app.route('/')
 def index():
@@ -28,8 +25,26 @@ def index():
 @app.route('/api/ip-status')
 def ip_status():
     if not check_ip:
-        return make_response('Lista vazia',204)
+        return make_response('Lista vazia', 204)
     return jsonify(check_ip)
+
+@app.route('/start-check/<string:vlan>', methods=['GET'])
+def check(vlan):
+    global background_thread, stop_event
+
+    # Acaba com a thread anterior se estiver rodando
+    if background_thread and background_thread.is_alive():
+        stop_event.set()  
+        background_thread.join()  
+
+    # recomeça o evento de parada
+    stop_event = threading.Event()
+
+    # Começa a nova thread
+    background_thread = threading.Thread(target=background_ip_check, args=(vlan,))
+    background_thread.start()
+
+    return jsonify({"status": "Search started", "vlan": vlan})
 
 if __name__ == '__main__':
     app.run(debug=True)
