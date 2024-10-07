79373c49e9fea857c78fdab92529bc4ab1e38e4e
phphengenharia <52513842+phphengenharia@users.noreply.github.com>
2024-10-07 11:51:21 -0300

    OK

--- app/ip_operations.py
+++ app/ip_operations.py
@@ -3,35 +3,39 @@ from ping3 import ping
 from collections import deque
 import json
 
-
 def verificar_ips(rede_base: str):
     ip_list = [rede_base + str(i) for i in range(1, 255)]
     ip_history = deque(maxlen=10)
     
     print(f"Verificando rede base n° {rede_base}.")
     
-    for i in range(0,10):
+    # Inicializar o histórico com "off" para cada IP
+    for i in range(0, 10):
         ip_history.append("off")
     
-    # Dicionario para armazenar o historico de status de cada IP
-    ip_status_dict = {status_history: ip_history.copy() for status_history in ip_list}
+    # Dicionário para armazenar o histórico de status de cada IP com instâncias individuais
+    ip_status_dict = {ip: deque(["off"] * 10, maxlen=10) for ip in ip_list}
 
-    # Dicionario para armazenar o status de cada IP apos verificao
+    # Dicionário para armazenar o status de cada IP após verificação
     ip_checked = {ip: "on" for ip in ip_list}
 
     def verificar_ip(ip):
-        print(f"IP n° {ip} -" , end=" ")
+        print(f"Verificando IP n° {ip}.")
         if ping(ip, timeout=5): 
             ip_status_dict[ip].append("on")
-        
+        else:
+            ip_status_dict[ip].append("off")
+
+    # Executar a verificação de IPs de forma concorrente
     with concurrent.futures.ThreadPoolExecutor(max_workers=255) as executor:
         executor.map(verificar_ip, ip_list)
 
+    # Atualizar o status final de cada IP após a verificação
     for ip in ip_list:
-            if ip_status_dict[ip].count("on") == 0:
-                ip_checked[ip] = "off"
-            else:
-                ip_checked[ip] = "on"   
+        if ip_status_dict[ip].count("on") == 0:
+            ip_checked[ip] = "off"
+        else:
+            ip_checked[ip] = "on"
 
     vlan = rede_base.split('.')[2]
     all_vlan_list = vlan_loader()
@@ -41,21 +45,19 @@ def verificar_ips(rede_base: str):
     if vlan_list != None:
         for item in ip_status_list:
             for vlan in vlan_list:
-                    if item['ip'] == vlan['ip']:
-                        item['descricao'] = vlan['descricao']
-                        break
-                    else:
-                        item['descricao'] = '-'
+                if item['ip'] == vlan['ip']:
+                    item['descricao'] = vlan['descricao']
+                    break
+            else:
+                item['descricao'] = '-'
     else:
         for item in ip_status_list:
             item['descricao'] = '-'
 
     return ip_status_list
 
-
 def vlan_loader():
     file_path = 'ips_list.json'
-    with open(file_path, 'r', encoding = 'utf-8') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
-    return data    
-
+    return data
