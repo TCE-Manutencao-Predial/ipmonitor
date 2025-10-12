24db06b053bfc27dcb052e534b1a4c512dee4e64
phphengenharia <52513842+phphengenharia@users.noreply.github.com>
2025-10-12 17:21:07 -0300

    refactor: Simplify Flask app initialization and enhance navigation button functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,7 @@
 import os
 from flask import Flask
 
-# Configurar os caminhos explicitamente
-template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
-static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
-
-app = Flask(__name__, 
-           template_folder=template_dir, 
-           static_folder=static_dir,
-           static_url_path='/static')
+app = Flask(__name__)
 
 from app import routes
 
